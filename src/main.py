98a1ef68216eae6cbc4c71f7b1b3526854c88386
fabiankastner <kastner.fabian@gmx.at>
2021-01-27 21:13:58 +0100

    some changes

--- src/main.py
+++ src/main.py
@@ -43,13 +43,13 @@ def preprocess_data(data_dir):
 
         X_image = Image.open(os.path.join(data_dir, X_file_name))
         X_image_resized = X_image.resize(size, Image.ANTIALIAS)
-        X_image_resized.save("data/images/{0}.png".format(index))
+        X_image_resized.save("data/images/img/{0}.png".format(index))
 
         y_image = Image.open(os.path.join(data_dir, y_file_name))
         y_image_resized = y_image.resize(size, Image.ANTIALIAS)
         y_image_resized_grayscale = ImageOps.grayscale(y_image_resized)
         y_image_resized_grayscale_binary = y_image_resized_grayscale.point(lambda p: p > 0 and 255) 
-        y_image_resized_grayscale_binary.save("data/masks/{0}.png".format(index))
+        y_image_resized_grayscale_binary.save("data/masks/img/{0}.png".format(index))
 
 
 def get_train_generator():
@@ -76,8 +76,8 @@ def get_train_generator():
         zoom_range=[0.5, 1],
         fill_mode="reflect")
 
-    image_generator =image_datagen.flow_from_directory("data/images", class_mode=None, seed=seed)
-    mask_generator = mask_datagen.flow_from_directory("data/masks", class_mode=None, seed=seed)
+    image_generator = image_datagen.flow_from_directory("data/images/", target_size=size, class_mode=None, seed=seed)
+    mask_generator = mask_datagen.flow_from_directory("data/masks/", target_size=size, color_mode='grayscale', class_mode=None, seed=seed)
     
     train_generator = zip(image_generator, mask_generator)
 
@@ -91,10 +91,14 @@ def main():
     train_generator = get_train_generator()
     model = UNet(size)
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
-    model.fit()
     print(model.summary())
+
+    model.fit(train_generator, steps_per_epoch=167,epochs=10)
+
+    
 
 
 if __name__ == "__main__":
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
     print("GPU") if len(tf.config.experimental.list_physical_devices('GPU')) > 0 else print("CPU")
     main()
